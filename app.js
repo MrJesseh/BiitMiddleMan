958ea3e3256487843212cd76b363eed392465805
MrJesseh <wjesse12@gmail.com>
2023-02-22 17:20:18 -0500

    testing

--- app.js
+++ app.js
@@ -131,7 +131,7 @@ app.get('/api/getEntry/:universeId/:dataStoreName/:entryKey/:scope', async funct
 
     //* Format and send the data.
     try{
-        let data = await response;
+        let data = await response.data;
         res.send(data);
     }catch(error){
         res.send({response: response, error: error});
